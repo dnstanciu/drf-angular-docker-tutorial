0a6ac82d5990c9fa460bc4460a798bb9e837772d
Dragos Stanciu <dragos.n.stanciu@gmail.com>
2018-12-07 13:01:22 +0200

    added POST method with APIView

--- django/todoapi/views.py
+++ django/todoapi/views.py
@@ -1,11 +1,28 @@
 from .models import Task
 from .serializers import TaskSerializer
 
-from rest_framework import generics
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-class TaskList(generics.ListAPIView):
+class TaskList(APIView):
     """
     View all tasks.
     """
-    queryset = Task.objects.all()
-    serializer_class = TaskSerializer
+    def get(self, request, format=None):
+        """
+        Return a list of all tasks.
+        """
+        tasks = Task.objects.all()
+        serializer = TaskSerializer(tasks, many=True)
+        return Response(serializer.data)
+
+    def post(self, request, format=None):
+        """
+        Create a task.
+        """
+        serializer = TaskSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
