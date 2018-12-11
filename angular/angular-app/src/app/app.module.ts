254445f44917fc3b8a1a5366bc1b0d562bc9baca
Dragos Stanciu <dragos.n.stanciu@gmail.com>
2018-12-11 13:19:04 +0200

    Handle GET requests to retrieve tasks from the API

--- angular/angular-app/src/app/app.module.ts
+++ angular/angular-app/src/app/app.module.ts
@@ -1,7 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { TaskListComponent } from './task-list/task-list.component';
 
@@ -12,7 +14,8 @@ import { TaskListComponent } from './task-list/task-list.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
