dea6f51058a1ccb53bb8b0a52216f16a1aeea64b
Dragos Stanciu <dragos.n.stanciu@gmail.com>
2018-12-15 15:17:11 +0200

    Added task creation form and POSTing to API

--- angular/angular-app/src/app/app.module.ts
+++ angular/angular-app/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 
@@ -15,7 +16,8 @@ import { TaskListComponent } from './task-list/task-list.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- angular/angular-app/src/app/task-list/task-list.component.ts
+++ angular/angular-app/src/app/task-list/task-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
+import { FormGroup, FormBuilder } from '@angular/forms';
 
 import { ApiService } from '../api.service';
 import { Task } from '../task';
@@ -11,15 +12,32 @@ import { Task } from '../task';
 })
 export class TaskListComponent implements OnInit {
   tasks$: Observable<Task[]>;
+  task_form: FormGroup;
 
-  constructor(private apiService: ApiService) { }
+  constructor(private apiService: ApiService, private form_builder: FormBuilder) { }
 
   ngOnInit() {
     this.getTasks();
+
+    this.task_form = this.form_builder.group({
+      title: '',
+      content: ''
+    });
   }
 
   public getTasks() {
     this.tasks$ = this.apiService.getTasks();
+  }
+
+  onSubmit() {
+    // Create the Task.
+    this.apiService.postTask(this.task_form.value)
+      .subscribe(
+        (response) => {
+          console.log(response);
+          this.getTasks();
+        }
+      )
   }
 
 }
