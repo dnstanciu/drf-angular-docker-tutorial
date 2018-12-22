fbfb971e12087ad1861eec51f8ed7a8acd6c0eb4
Dragos Stanciu <dragos.n.stanciu@gmail.com>
2018-12-22 13:58:18 +0200

    Added user information to Tasks and added filtering for List view

--- django/todoapi/views.py
+++ django/todoapi/views.py
@@ -3,6 +3,9 @@ from .serializers import TaskSerializer
 
 from rest_framework import generics
 
+from .utils import get_auth0_user_id_from_request
+
+# Lists and Creates entries of Task.
 class TaskList(generics.ListCreateAPIView):
     """
     Lists and creates tasks.
@@ -10,6 +13,20 @@ class TaskList(generics.ListCreateAPIView):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
 
+    def perform_create(self, serializer):
+        auth0_user_id = get_auth0_user_id_from_request(self.request)
+        # Set the user to the one in the token.
+        serializer.save(created_by=auth0_user_id)
+
+    def get_queryset(self):
+        """
+        This view should return a list of all Tasks
+        for the currently authenticated user.
+        """
+        auth0_user_id = get_auth0_user_id_from_request(self.request)
+        return Task.objects.filter(created_by=auth0_user_id)
+
+# Return a single Task and allows its update and deletion.
 class TaskDetail(generics.RetrieveUpdateDestroyAPIView):
     """
     Returns a single Task and allows updates and deletion of a Task.
